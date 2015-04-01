ded006d5b2ac7b58c1e78af6faa253b112b00eef
Tony Han <h.bing612@gmail.com>
2015-04-02 02:09:21 +0800

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,31 +1,42 @@
 defmodule Qiniu.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :qiniu,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+
+     # Hex
+     description: description,
+     package: package
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:poison, "~> 1.3.1"},
-     {:httpoison, "~> 0.6.2"}]
+     {:httpoison, "~> 0.6.2"},
+     {:ex_doc, "~> 0.7", only: :docs},
+     {:earmark, "~> 0.1", only: :docs},
+     {:inch_ex, only: :docs}
+    ]
   end
+
+  defp description do
+    "Qiniu Resource (Cloud) Storage SDK for Elixir"
+  end
+
+  defp package do
+    [contributors: ["Tony Han"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/tony612/qiniu"},
+     files: ~w(mix.exs README.md CHANGELOG.md lib config)]
+  end
+
 end
