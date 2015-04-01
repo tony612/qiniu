785b6e7823db300d69d36f3444e7df1b8a85e4bb
Tony Han <h.bing612@gmail.com>
2015-04-02 01:33:27 +0800

    refactor PutPolicy to add doc and spec

--- lib/qiniu/put_policy.ex
+++ lib/qiniu/put_policy.ex
@@ -1,25 +1,67 @@
 defmodule Qiniu.PutPolicy do
-  defstruct scope:                 "scope",
-            save_key:              "saveKey",
-            end_user:              "endUser",
-            return_url:            "returnUrl",
-            return_body:           "returnBody",
-            callback_url:          "callbackUrl",
-            callback_host:         "callbackHost",
-            callback_body:         "callbackBody",
-            callback_body_type:    "callbackBodyType",
-            persistent_ops:        "persistentOps",
-            persistent_notify_url: "persistentNotifyUrl",
-            persistent_pipeline:   "persistentPipeline",
+  @moduledoc """
+  PutPolicy struct for uploading. And you have to specify right `scope`
+  and `deadline` to let uploading work.
 
-            deadline:              "deadline",
-            insert_only:           "insertOnly",
-            fsize_limit:           "fsizeLimit",
-            callback_fetch_key:    "callbackFetchKey",
-            detect_mime:           "detectMime",
-            mime_limit:            "mimeLimit"
+  See http://developer.qiniu.com/docs/v6/api/reference/security/put-policy.html
+  """
 
-  def to_json(%Qiniu.PutPolicy{} = policy) do
+  alias __MODULE__
+
+  defstruct scope:                 nil,
+            deadline:              nil,
+            insert_only:           nil,
+            save_key:              nil,
+            end_user:              nil,
+            return_url:            nil,
+            return_body:           nil,
+            callback_url:          nil,
+            callback_host:         nil,
+            callback_body:         nil,
+            callback_body_type:    nil,
+            callback_fetch_key:    nil,
+            persistent_ops:        nil,
+            persistent_notify_url: nil,
+            persistent_pipeline:   nil,
+            fsize_limit:           nil,
+            detect_mime:           nil,
+            mime_limit:            nil,
+            checksum:              nil
+
+  @type t :: %PutPolicy{
+            scope:                 String.t,
+            deadline:              integer,
+            insert_only:           integer | nil,
+            save_key:              String.t | nil,
+            end_user:              String.t | nil,
+            return_url:            String.t | nil,
+            return_body:           String.t | nil,
+            callback_url:          String.t | nil,
+            callback_host:         String.t | nil,
+            callback_body:         String.t | nil,
+            callback_body_type:    String.t | nil,
+            callback_fetch_key:    integer | nil,
+            persistent_ops:        String.t | nil,
+            persistent_notify_url: String.t | nil,
+            persistent_pipeline:   String.t | nil,
+            fsize_limit:           integer | nil,
+            detect_mime:           integer | nil,
+            mime_limit:            String.t | nil,
+            checksum:              String.t | nil
+            }
+
+
+  @doc """
+  Change put_policy to json string expect the `nil` values.
+
+  ## Examples
+
+      iex> policy = %Qiniu.PutPolicy{scope: "scope", deadline: 1427990400}
+      iex> Qiniu.PutPolicy.to_json(policy)
+      ~s({"scope":"scope","deadline":1427990400})
+  """
+  @spec to_json(PutPolicy.t) :: String.t
+  def to_json(%PutPolicy{} = policy) do
     policy
       |> Map.from_struct
       |> Enum.filter(fn {_, v} -> v != nil end)
@@ -27,7 +69,17 @@ defmodule Qiniu.PutPolicy do
       |> Poison.encode!
   end
 
-  def encoded_json(%Qiniu.PutPolicy{} = policy) do
+  @doc """
+  Change put_policy to json string and encode it with url safe
+
+  ## Examples
+
+      iex> policy = %Qiniu.PutPolicy{scope: "scope"}
+      iex> Qiniu.PutPolicy.encoded_json(policy)
+      "eyJzY29wZSI6InNjb3BlIn0="
+  """
+  @spec encoded_json(PutPolicy.t) :: String.t
+  def encoded_json(%PutPolicy{} = policy) do
     policy |> to_json |> Base.url_encode64
   end
 end
--- test/qiniu/put_policy_test.exs
+++ test/qiniu/put_policy_test.exs
@@ -1,15 +1,5 @@
 defmodule Qiniu.PutPolicyTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  alias Qiniu.PutPolicy
-
-  test "to_json returns json string with nil" do
-    policy = %{__struct__: PutPolicy, scope: "scope", save_key: nil}
-    assert PutPolicy.to_json(policy) == "{\"scope\":\"scope\"}"
-  end
-
-  test "encoded_json" do
-    policy = %{__struct__: PutPolicy, scope: "scope"}
-    assert PutPolicy.encoded_json(policy) == "eyJzY29wZSI6InNjb3BlIn0="
-  end
+  doctest Qiniu.PutPolicy
 end
