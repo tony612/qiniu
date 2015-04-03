ef715c7280116aa0a810941ef305ff66b63c0300
Tony Han <h.bing612@gmail.com>
2015-04-03 22:51:43 +0800

    delete config for different env

--- config/config.exs
+++ config/config.exs
@@ -4,10 +4,5 @@ config :qiniu, Qiniu,
   user_agent:       "QiniuElixir/#{Qiniu.Mixfile.project[:version]}/Ruby/#{System.version}",
   content_type:     "application/x-www-form-urlencoded",
   up_host:          "http://up.qiniu.com",
-  access_key:       "",
-  secret_key:       ""
-
-
-if Mix.env == :test || Mix.env == :dev do
-  import_config "#{Mix.env}.exs"
-end
+  access_key:       "key",
+  secret_key:       "secret"
--- config/test.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :qiniu, Qiniu, access_key: "key", secret_key: "secret"
