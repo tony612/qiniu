e7db1f345bfe9f67a141fe5098b9591f6ec36184
Tony Han <h.bing612@gmail.com>
2015-04-04 22:24:48 +0800

    make the Qiniu.config better

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,3 @@
 use Mix.Config
 
-config :qiniu, Qiniu,
-  user_agent:       "QiniuElixir/#{Qiniu.Mixfile.project[:version]}/Ruby/#{System.version}",
-  content_type:     "application/x-www-form-urlencoded",
-  up_host:          "http://up.qiniu.com",
-  access_key:       "key",
-  secret_key:       "secret"
+config :qiniu, Qiniu, access_key: "key", secret_key: "secret"
--- lib/qiniu.ex
+++ lib/qiniu.ex
@@ -1,5 +1,13 @@
 defmodule Qiniu do
   def config do
-    Application.get_env(:qiniu, Qiniu)
+    Keyword.merge(default_config, Application.get_env(:qiniu, Qiniu, []))
+  end
+
+  defp default_config do
+    [
+      user_agent:       "QiniuElixir/#{Qiniu.Mixfile.project[:version]}/Ruby/#{System.version}",
+      content_type:     "application/x-www-form-urlencoded",
+      up_host:          "http://up.qiniu.com",
+    ]
   end
 end
