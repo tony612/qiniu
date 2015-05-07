e6e98e0554e2c16a0e9bf3e5cddc73188719a81e
Tony Han <h.bing612@gmail.com>
2015-05-07 20:38:00 +0800

    support headers and parse options in HTTP.post

--- lib/qiniu/http.ex
+++ lib/qiniu/http.ex
@@ -1,13 +1,19 @@
 defmodule Qiniu.HTTP do
   @moduledoc false
 
-  def post(url, body) do
-    headers = [
+  def post(url, body, opts \\ []) do
+    headers = Keyword.merge(opts[:headers] || [], [
       accept: "application/json",
       connection: "close",
       user_agent: Qiniu.config[:user_agent],
-    ]
+    ])
 
-    HTTPoison.post! url, body, headers
+    response = HTTPoison.post! url, body, headers
+
+    if opts[:parse] do
+      %{response | body: Poison.decode!(response.body)}
+    else
+      response
+    end
   end
 end
