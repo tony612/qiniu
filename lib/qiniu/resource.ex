73e7f08456e60a5fe32d4207fe7710ec654a2d36
Tony Han <h.bing612@gmail.com>
2015-05-08 22:16:42 +0800

    refactor stat and add doc for it

--- lib/qiniu/resource.ex
+++ lib/qiniu/resource.ex
@@ -5,14 +5,21 @@ defmodule Qiniu.Resource do
   See http://developer.qiniu.com/docs/v6/api/reference/rs/
   """
 
-  def stat(bucket, key) do
-    encoded_entry = Base.url_encode64(bucket <> ":" <> key)
+  @doc """
+  Get the metadata of the resource
+
+  ## Fields
+
+    * `entry_uri` - uri of your resource entry, "<bucket>:<key>"
+  """
+  def stat(entry_uri) do
+    encoded_entry = Base.url_encode64(entry_uri)
     url = Path.join([Qiniu.config[:rs_host], "stat", encoded_entry])
-    auth_post(url, "")
+    auth_post(url)
   end
 
-  defp auth_post(url, body) do
-    Qiniu.HTTP.post url, body, parse: true, headers: [
+  defp auth_post(url, body \\ "") do
+    Qiniu.HTTP.post url, body, headers: [
       Authorization: "QBox " <> Qiniu.Auth.access_token(url, body)
     ]
   end
