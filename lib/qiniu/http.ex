77bed5f2dea1b66b6b30388f063f71883c7d6112
Tony Han <h.bing612@gmail.com>
2015-05-13 22:05:03 +0800

    refactor the HTTP module to support GET

--- lib/qiniu/http.ex
+++ lib/qiniu/http.ex
@@ -2,13 +2,27 @@ defmodule Qiniu.HTTP do
   @moduledoc false
 
   def post(url, body, opts \\ []) do
+    request(:post, url, body, opts)
+  end
+
+  def get(url, opts \\ []) do
+    request(:get, url, opts)
+  end
+
+  defp request(method, url, opts) do
+    request(method, url, "", opts)
+  end
+  defp request(method, url, body, opts) do
     headers = Keyword.merge(opts[:headers] || [], [
       accept: "application/json",
       connection: "close",
       user_agent: Qiniu.config[:user_agent],
     ])
 
-    response = HTTPoison.post! url, body, headers
+    response = case method do
+      :get -> HTTPoison.get! url, headers
+      :post -> HTTPoison.post! url, body, headers
+    end
 
     if opts[:parse] do
       %{response | body: Poison.decode!(response.body)}
