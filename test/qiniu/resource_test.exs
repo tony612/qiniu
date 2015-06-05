f27a05c0c3f3c1f738551d0071894c406205bea3
Tony Han <h.bing612@gmail.com>
2015-06-05 21:44:31 +0800

    add tests for copy, move and delete of Resource

--- test/qiniu/resource_test.exs
+++ test/qiniu/resource_test.exs
@@ -14,4 +14,28 @@ defmodule Qiniu.ResourceTest do
       end
     end
   end
+
+  test "copy" do
+    with_mock Auth, [access_token: fn("http://rs.qiniu.com/copy/Yjpr/YjE6azE=", "") -> "access_token" end] do
+      with_mock HTTP, [post: fn("http://rs.qiniu.com/copy/Yjpr/YjE6azE=", "", headers: [Authorization: "QBox access_token"]) -> "response" end] do
+        assert Qiniu.Resource.copy("b:k", "b1:k1") == "response"
+      end
+    end
+  end
+
+  test "move" do
+    with_mock Auth, [access_token: fn("http://rs.qiniu.com/move/Yjpr/YjE6azE=", "") -> "access_token" end] do
+      with_mock HTTP, [post: fn("http://rs.qiniu.com/move/Yjpr/YjE6azE=", "", headers: [Authorization: "QBox access_token"]) -> "response" end] do
+        assert Qiniu.Resource.move("b:k", "b1:k1") == "response"
+      end
+    end
+  end
+
+  test "delete" do
+    with_mock Auth, [access_token: fn("http://rs.qiniu.com/delete/Yjpr", "") -> "access_token" end] do
+      with_mock HTTP, [post: fn("http://rs.qiniu.com/delete/Yjpr", "", headers: [Authorization: "QBox access_token"]) -> "response" end] do
+        assert Qiniu.Resource.delete("b:k") == "response"
+      end
+    end
+  end
 end
